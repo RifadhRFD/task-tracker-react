326fbd37d3120f2cdfd5d4e64b2c544029ff064d
RifadhRFD <rifadhrfd@gmail.com>
2022-12-07 00:18:36 +0530

    adding task file

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,30 +1,35 @@
 
-const tasks = [
-  {
-    id: 1,
-    text: 'Doctor\'s Appointment',
-    day: 'Feb 5th at 2.30pm',
-    reminder: true,
-  },
-  {
-    id: 1,
-    text: 'Teacher\'s Appointment',
-    day: 'Feb 6th at 3.30pm',
-    reminder: true,
-  },
-  {
-    id: 1,
-    text: 'Student\'s Appointment',
-    day: 'Feb 7th at 4.30pm',
-    reminder: true,
-  }
-];
+import { useState } from "react";
+
+
 
 const Task = () => {
+  const [tasks, setTasks] = useState([
+      {
+        id: 1,
+        text: 'Doctor\'s Appointment',
+        day: 'Feb 5th at 2.30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Teacher\'s Appointment',
+        day: 'Feb 6th at 3.30pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Student\'s Appointment',
+        day: 'Feb 7th at 4.30pm',
+        reminder: true,
+      }
+    
+  ])
   return (
     <>
-      {tasks.map((task) => ( <h3>{task.text}</h3> )
-      
+      {tasks.map((task) => ( 
+      <h3 key={task.id}>{task.text}</h3> 
+      )
       )}
     </>
   )
