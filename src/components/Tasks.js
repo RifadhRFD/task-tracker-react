b17a9a1be07a33094566cefff7fc234786231233
RifadhRFD <rifadhrfd@gmail.com>
2022-12-06 23:52:19 +0530

    altering task component data

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -9,13 +9,13 @@ const tasks = [
   {
     id: 1,
     text: 'Teacher\'s Appointment',
-    day: 'Feb 6th at 2.30pm',
+    day: 'Feb 6th at 3.30pm',
     reminder: true,
   },
   {
     id: 1,
     text: 'Student\'s Appointment',
-    day: 'Feb 7th at 2.30pm',
+    day: 'Feb 7th at 4.30pm',
     reminder: true,
   }
 ];
